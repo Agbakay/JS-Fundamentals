89117b88058a1b772c5bfaec660f218d31c71dc3
Agbakay <kayodeakintunde919@gmail.com>
2025-05-24 23:54:48 +0100

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,1 +1,8 @@
-
+const argsLength = process.argv.Lenth -2;
+if(argsLength === 0) {
+  console.log("No argument");
+} else if (argsLength ===1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
