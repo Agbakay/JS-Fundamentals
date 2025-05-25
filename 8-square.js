bfe48b683869b96e2e2fb18c5a44d133b8f18bb0
Agbakay <kayodeakintunde919@gmail.com>
2025-05-25 19:56:04 +0100

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,1 +1,13 @@
-
+const sizeArg = process.argv[2];
+const size = Number(sizeArg);
+if (!Number.isInteger(size) || size <= 0) {
+  console.log("Missing size");
+} else {
+  let row = '';
+  for (let i = 0; i < size; i++) {
+    row += 'X';
+  }
+  for (let i = 0; i < size; i++) {
+    console.log(row);
+  }
+}
