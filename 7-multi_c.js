2907900da9341e24989e9ceb82651b3a1e0966f7
Agbakay <kayodeakintunde919@gmail.com>
2025-05-25 19:45:29 +0100

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,10 @@
 const arg = process.argv[2];
 const x = Number(arg);
 
-if (Number.isInteger(x) && x > 0) {
+if (Number.isInteger(x) && x > 0) { 
   let i = 0;
   while (i < x) {
-    console.log("C is fun"); 
+    console.log("C is fun");
     i++;
   }
 } else {
