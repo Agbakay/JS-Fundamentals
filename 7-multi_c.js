85c47c3b19067c50e9c887e8000ac705714911a0
Agbakay <kayodeakintunde919@gmail.com>
2025-05-25 19:53:32 +0100

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,11 @@
 const arg = process.argv[2];
 const x = Number(arg);
-
 if (Number.isInteger(x) && x > 0) {
   let i = 0;
   while (i < x) {
-    console.log("C is fun"); 
+    console.log("C is fun");
     i++;
   }
-} else {
+} else if (isNaN(x) || arg === undefined) { 
   console.log("Missing number of occurrences");
 }
